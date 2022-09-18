19665ee617d544e9f145065a2124e01015545fb4
Yurii Bondarenko <bondarenkoyura@gmail.com>
2022-09-18 12:30:30 -0400

    Hashing

---
+++ src/test/java/com/bondarenko/algo/misc/HashingUtilTest.java
@@ -0,0 +1,19 @@
+package com.bondarenko.algo.misc;
+
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HashingUtilTest {
+
+	@Test
+	void hash() {
+		BigInteger hash1 = HashingUtil.hash("Yurii");
+		BigInteger hash2 = HashingUtil.hash("Yurii");
+		BigInteger hash3 = HashingUtil.hash("Carol");
+		assertThat(hash1).isEqualTo(hash2);
+		assertThat(hash2).isNotEqualTo(hash3);
+	}
+}
