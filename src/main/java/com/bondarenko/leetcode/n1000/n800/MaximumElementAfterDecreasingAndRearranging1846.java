39e553a69d5728dbfb29c8ad9feeb2918e076436
Yurii Bondarenko <bondarenkoyura@gmail.com>
2023-11-14 18:48:40 -0800

    Algo 1846

--- src/main/java/com/bondarenko/leetcode/n1000/n800/MaximumElementAfterDecreasingAndRearranging1846.java
+++ src/main/java/com/bondarenko/leetcode/n1000/n800/MaximumElementAfterDecreasingAndRearranging1846.java
@@ -1,21 +1,26 @@
 package com.bondarenko.leetcode.n1000.n800;
 
-import java.util.Arrays;
-
 /*
  URL: https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging
- Time: NLogN
- Space: 1
+ Time: N
+ Space: N
  */
 public class MaximumElementAfterDecreasingAndRearranging1846 {
 
 	public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-		Arrays.sort(arr);
-		arr[0] = 1;
-		for (int i = 1; i < arr.length; i++)
-			if (arr[i - 1] + 1 < arr[i])
-				arr[i] = arr[i - 1] + 1;
-		return arr[arr.length - 1];
+		var n = arr.length;
+		var seq = new int[n];
+		for (int v : arr)
+			seq[Math.min(v - 1, n - 1)]++;
+		for (int i = n - 1; i > 0; i--) {
+			seq[i-1] += Math.max(0, seq[i] - 1);
+			seq[i] = Math.min(1, seq[i]);
+		}
+		var cnt = 0;
+		for (int v : seq)
+			if (v > 0)
+				cnt++;
+		return cnt;
 	}
 
 }
