39ac248be5af0e298b1688d17ce3703815e18f2c
Yurii Bondarenko <bondarenkoyura@gmail.com>
2023-11-21 00:32:15 -0800

    Algo 1814

--- src/main/java/com/bondarenko/leetcode/n1000/n800/CountNicePairsInAnArray1814.java
+++ src/main/java/com/bondarenko/leetcode/n1000/n800/CountNicePairsInAnArray1814.java
@@ -1,34 +1,32 @@
 package com.bondarenko.leetcode.n1000.n800;
 
+import java.util.HashMap;
+
 /*
  URL: https://leetcode.com/problems/count-nice-pairs-in-an-array
- Time: x
- Space: x
+ Time: N
+ Space: N
  */
 public class CountNicePairsInAnArray1814 {
 
-	// x
+	private static final int MOD = 1000000007;
+
+	// array
 	public int countNicePairs(int[] nums) {
-		var adj = new int[nums.length];
-		for (int i = 0; i < nums.length; i++) {
-			var k = nums[i];
-			var r = 0;
-			while (k > 0) {
-				r = r * 10 + k % 10;
-				k /= 10;
-			}
-			adj[i] = nums[i] - r;
-		}
 		var ans = 0;
-		for (int i = 0; i < nums.length - 1; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (adj[i] == adj[j])
-					ans++;
-				if (ans == 1000000007)
-					ans = 0;
-			}
-		}
+		var cnt = new HashMap<Integer, Integer>();
+		for (int n : nums)
+			ans = (ans + cnt.merge(n - rev(n), 1, Integer::sum) - 1) % MOD;
 		return ans;
+	}
+
+	private static int rev(int k) {
+		var r = 0;
+		while (k > 0) {
+			r = r * 10 + k % 10;
+			k /= 10;
+		}
+		return r;
 	}
 
 }
