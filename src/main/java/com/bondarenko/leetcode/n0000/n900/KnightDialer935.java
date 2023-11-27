de8c211a93e39f850841cb02d094c2d0e4583041
Yurii Bondarenko <bondarenkoyura@gmail.com>
2023-11-26 19:18:09 -0800

    Algo 935

--- src/main/java/com/bondarenko/leetcode/n0000/n900/KnightDialer935.java
+++ src/main/java/com/bondarenko/leetcode/n0000/n900/KnightDialer935.java
@@ -1,17 +1,39 @@
 package com.bondarenko.leetcode.n0000.n900;
 
+import java.util.Arrays;
+
 /*
  URL: https://leetcode.com/problems/knight-dialer
- Time: x
- Space: x
+ Time: N
+ Space: 1
  */
 public class KnightDialer935 {
 
+	private static final int[][] MOVES =
+			{{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
+	private static final int DIGITS = 10;
 	private static final int MOD = 1000000007;
 
-	// x
+	// dp
 	public int knightDialer(int n) {
-		return 0;
+		var cur = new int[DIGITS];
+		var pre = new int[DIGITS];
+		Arrays.fill(pre, 1);
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < DIGITS; j++) {
+				var cnt = 0;
+				for (int m : MOVES[j])
+					cnt = (cnt + pre[m]) % MOD;
+				cur[j] = cnt;
+			}
+			var tmp = pre;
+			pre = cur;
+			cur = tmp;
+		}
+		var ans = 0;
+		for (int p : pre)
+			ans = (ans + p) % MOD;
+		return ans;
 	}
 
 }
