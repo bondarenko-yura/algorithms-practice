98c3d0727491cf6a150c07b87135d863a9aef41a
Yurii Bondarenko <bondarenkoyura@gmail.com>
2023-12-14 20:14:59 -0800

    Algo 1436

--- src/main/java/com/bondarenko/leetcode/n1000/n400/DestinationCity1436.java
+++ src/main/java/com/bondarenko/leetcode/n1000/n400/DestinationCity1436.java
@@ -14,14 +14,13 @@ public class DestinationCity1436 {
 
 	// iteration
 	public String destCity(List<List<String>> paths) {
-		var dest = new HashSet<String>();
-		for (List<String> route : paths)
-			dest.add(route.get(route.size() - 1));
-		for (List<String> route : paths)
-			for (int i = 0; i < route.size() - 1; i++)
-				dest.remove(route.get(i));
-		var it = dest.iterator();
-		return it.hasNext() ? it.next() : "";
+		var outgoing = new HashSet<String>();
+		for (List<String> path: paths)
+			outgoing.add(path.get(0));
+		for (List<String> path: paths)
+			if (!outgoing.contains(path.get(1)))
+				return path.get(1);
+		return "";
 	}
 
 }
