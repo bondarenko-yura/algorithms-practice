5769cd5f0daea86110954cc7551a51f7598fd799
Yurii Bondarenko <bondarenkoyura@gmail.com>
2023-12-17 10:35:16 -0800

    Algo 102

--- src/main/java/com/bondarenko/leetcode/n0000/n100/BinaryTreeLevelOrderTraversal102.java
+++ src/main/java/com/bondarenko/leetcode/n0000/n100/BinaryTreeLevelOrderTraversal102.java
@@ -1,5 +1,6 @@
 package com.bondarenko.leetcode.n0000.n100;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bondarenko.leetcode.ds.TreeNode;
@@ -11,9 +12,21 @@ import com.bondarenko.leetcode.ds.TreeNode;
  */
 public class BinaryTreeLevelOrderTraversal102 {
 
+	private static void dfs(TreeNode root, List<List<Integer>> ans, int lvl) {
+		if (root == null)
+			return;
+		if (ans.size() == lvl)
+			ans.add(new ArrayList<>());
+		ans.get(lvl).add(root.val);
+		dfs(root.left, ans, lvl + 1);
+		dfs(root.right, ans, lvl + 1);
+	}
+
 	// dfs
 	public List<List<Integer>> levelOrder(TreeNode root) {
-		return null;
+		var ans = new ArrayList<List<Integer>>();
+		dfs(root, ans, 0);
+		return ans;
 	}
 
 }
