ac031ddcb6d02f41379aff74dbed6f531ff6c0cc
Yurii Bondarenko <bondarenkoyura@gmail.com>
2024-07-23 22:46:23 -0700

    Algo 2191

--- src/main/java/com/bondarenko/leetcode/n2000/n100/SortTheJumbledNumbers2191.java
+++ src/main/java/com/bondarenko/leetcode/n2000/n100/SortTheJumbledNumbers2191.java
@@ -11,21 +11,23 @@ import java.util.Comparator;
 public class SortTheJumbledNumbers2191 {
 	public int[] sortJumbled(int[] mapping, int[] nums) {
 		var jumbled = new int[nums.length][];
-		for (int i = 0; i < nums.length; i++) {
-			var mult = 1;
-			var n = nums[i];
-			var v = 0;
-			do {
-				v += mapping[n % 10] * mult;
-				n /= 10;
-				mult *= 10;
-			} while (n > 0);
-			jumbled[i] = new int[] {v, i, nums[i]};
-		}
+		for (int i = 0; i < nums.length; i++)
+			jumbled[i] = new int[] {convert(nums[i], mapping), i, nums[i]};
 		Arrays.sort(jumbled, Comparator.comparingInt((int[] a) -> a[0]).thenComparingInt(a -> a[1]));
 		var ans = new int[nums.length];
 		for (int i = 0; i < jumbled.length; i++)
 			ans[i] = jumbled[i][2];
 		return ans;
+	}
+
+	private static int convert(int n, int[] mapping) {
+		var mult = 1;
+		var v = 0;
+		do {
+			v += mapping[n % 10] * mult;
+			n /= 10;
+			mult *= 10;
+		} while (n > 0);
+		return v;
 	}
 }
